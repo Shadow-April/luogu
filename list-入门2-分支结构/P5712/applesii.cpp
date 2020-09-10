588c8bdcb6dddc4665a6b454143179659177441f
aprildykj <aprildykj@gmail.com>
2020-09-10 21:13:49 +0800

    :seedling: hua_hua

---
+++ list-入门2-分支结构/P5712/applesii.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: applesii.cpp
+	> Author: april
+	> Mail: aprildykj@gmail.com 
+	> Created Time: Thu, Sep 10, 2020  9:07:14 PM
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a;
+	cin>>a;
+	cout<<"Today, I ate "<<a<<" apple";
+	if(a>1)
+		cout<<"s.\n";
+	else
+		cout<<".\n";
+	return 0;
+}
