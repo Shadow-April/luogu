1a7e0fa92d4bf21d9139a11db9efa9b6a30dbd3e
aprildykj <aprildykj@gmail.com>
2020-09-11 21:32:51 +0800

    :hammer: local_luogu

---
+++ list-入门2-分支结构/P5713/local.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: local.cpp
+	> Author: april
+	> Mail: aprildykj@gmail.com 
+	> Created Time: Fri, Sep 11, 2020  9:24:50 PM
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a<=5){
+		printf("Local\n");
+	} else {
+		printf("Luogu\n");
+	}
+	return 0;
+}
+
