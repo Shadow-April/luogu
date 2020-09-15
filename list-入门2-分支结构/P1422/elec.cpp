01873766d9edac2fa917c723736006e32b616227
aprildykj <aprildykj@gmail.com>
2020-09-15 23:35:01 +0800

    :seedling: uning...

---
+++ list-入门2-分支结构/P1422/elec.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: elec.cpp
+	> Author: april
+	> Mail: aprildykj@gmail.com 
+	> Created Time: Tue, Sep 15, 2020 11:31:08 PM
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	double p;
+	int n;
+	scanf("%d", &n);
+	if(n>400)
+		s=150*0.4463+250*0.4663+(n-)
